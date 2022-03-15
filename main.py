60f0c07d900d00f7df0e73155699d8e2c0f5676f
Genius1512 <79528697+Genius1512@users.noreply.github.com>
2022-03-15 18:49:36 +0000

    Update

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
 import configparser
 import os
 
+import timetable as tt
+
 
 def init_ttasbackground():
-	os.mkdir(".ttasbackground/")
+    os.mkdir(".ttasbackground/")  # TODO: point to ~/.ttasbackground
 
 
 def get_config(path):
-	if not os.path.exists(path):
-		make_config(path)
+    if not os.path.exists(path):
+    	make_config(path)
 
-	config = configparser.ConfigParser(interpolation=None)
+    config = configparser.ConfigParser(interpolation=None)
 
-	config.read(path)
-	return config
+    config.read(path)
+    return config
 
 
 def make_config(path):
 	if not os.path.exists(os.path.split(path)[1]):
 		init_ttasbackground()
-	
+
 	hash = input("Enter hash: ")
 	auth_key = input("Enter auth key: ")
+	id = input("Enter id: ")
 
 	with open(path, "w") as f:
 		f.write(f"""[Creds]
 hash={hash}
-auth_key={auth_key}""")
+auth_key={auth_key}
+id={id}""")
 
 
 def main():
-	config = get_config("config.ini")
-	print(config)
+	config = get_config("config.ini")  # TODO: correct path
+
+    # TODO: fetch pdf
+	tt.fetch_pdf(
+		config["Creds"]["id"],
+		tt.get_date(),
+		tt.get_sturm_session(hash),
+		"tt.pdf"
+	)
+	# TODO: convert to png
+	# TODO: set as background
 
 
 main()
---
+++ timetable.py
@@ -0,0 +1,56 @@
+import requests as req
+import convertapi as capi
+import time as t
+
+
+def to_image(path, auth_key):
+	"""
+	path without ending, ex.: timetable.
+	"""
+	
+	capi.api_secret = auth_key
+	capi.convert(
+		"png",
+		{
+			"File": path+"pdf"
+		}
+	).file.save(path + "png")
+
+
+def set_as_background(path):
+	print("Setting as background")
+
+
+def fetch_pdf(id, time, cookie, path):
+    # URL to replicate: https://intranet.tam.ch/ksl/print/pdf-timetable?filter=student&id=4020344&start=2022-2-28&table=week&daysviewed=week&reservations=0
+    url = f"https://intranet.tam.ch/ksl/print/pdf-timetable?filter=student&id={id}&start={time}&table=week&daysviewed=week&reservations=0"
+    out = req.get(url, cookies=cookie)
+    with open(path, "wb") as f:
+        f.write(out.content)
+
+
+def get_sturm_session(hash: str):
+	url = f"https://intranet.tam.ch/ksl/rest/ics/type/timetable/date/1642582786/auth/{hash}/calendar.ics"
+	resp = req.get(url)
+	return resp.cookies
+
+
+def get_date():
+    time = t.strftime("%Y-%m-")
+    today = t.strftime("%a")
+    
+    if today == "Mon":
+        time += t.strftime("%d")
+    elif today == "Tue":
+        time += str(int(t.strftime("%d"))-1)
+    elif today == "Wed":
+        time += str(int(t.strftime("%d"))-2)
+    elif today == "Thu":
+        time += str(int(t.strftime("%d")-3))
+    elif today == "Fri":
+        time += str(int(t.strftime("%d"))-4)
+    elif today == "Sat":
+        time += str(int(t.strftime("%d"))+2)
+    elif today == "Sun":
+        time += str(int(t.strftime("%d"))+1)
+    return time
